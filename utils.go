583042c3c73120fca81068ac528f75a358e4b7f1
charles <unclebigxc2013@gmail.com>
2017-03-13 13:54:44 +0800

    update add func write append append to a file

--- utils.go
+++ utils.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"crypto/tls"
 	"fmt"
+	"os"
 	"sort"
 	"strconv"
 	"strings"
@@ -99,4 +100,19 @@ func SortMaps(sortMap map[string]string, appKey string) (sortedStr string) {
 	}
 
 	return
+}
+
+func appendToFile(fileName string, content string) error {
+	// 以只写的模式，打开文件
+	f, err := os.OpenFile(fileName, os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("cacheFileList.yml file create failed. err: " + err.Error())
+	} else {
+		// 查找文件末尾的偏移量
+		n, _ := f.Seek(0, os.SEEK_END)
+		// 从末尾的偏移量开始写入内容
+		_, err = f.WriteAt([]byte(content), n)
+	}
+	defer f.Close()
+	return err
 }
