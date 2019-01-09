55f5352531af869d4b4b726ffc6f1b4cd3abf03e
unclebig <unclebigxc2013@gmail.com>
2019-01-09 14:10:15 +0800

    add func generate a random string with specific length

--- utils.go
+++ utils.go
@@ -124,8 +124,6 @@ func GenerateRandnumWithin(max int) int {
 	rand.Seed(time.Now().Unix())
 	randNum := rand.Intn(max)
 
-	fmt.Printf("rand is %v\n", randNum)
-
 	return randNum
 }
 
@@ -134,7 +132,7 @@ func GenerateRangeNumBetween(min, max int) int {
 	rand.Seed(time.Now().Unix())
 	randNum := rand.Intn(max - min)
 	randNum = randNum + min
-	fmt.Printf("rand is %v\n", randNum)
+
 	return randNum
 }
 
@@ -151,4 +149,17 @@ func appendToFile(fileName string, content string) error {
 	}
 	defer f.Close()
 	return err
+}
+
+//
+//GenRandomString generate a random string with specific length
+func GenRandomString(length int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyz"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < length; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
 }
